13b51ba520abb1a884b29f424ca80a05552f769c
unknown <genf2605@DINF-1017-18A.DInf.FSCI.USherbrooke.ca>
2013-07-29 14:44:47 -0400

    AbstractFoctory of the maze

---
+++ IDude.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Theseus
+{
+    public interface IDude : IDrawable
+    {
+        public Point Coord { get; set; }
+    }
+}
