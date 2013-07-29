368706e4168aa549e7214589abc5e31823d757d6
unknown <genf2605@DINF-1017-18A.DInf.FSCI.USherbrooke.ca>
2013-07-29 14:29:12 -0400

    sup stuff this shit up

---
+++ theseus/theseus/IDude.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Theseus
+{
+    interface IDude : IDrawable
+    {
+        public Point Coord { get; set; }
+    }
+}
